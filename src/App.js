cd790da0e42dd184e4e8cd842c7207df085a409a
Yann Mainier <yann.mainier@gmail.com>
2021-01-27 16:56:52 +0000

    Adding loading and error management

--- src/App.js
+++ src/App.js
@@ -6,29 +6,39 @@ const API = "https://pokeapi.co/api/v2";
 function usePokeApi() {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
   const [url, setUrl] = useState("");
   useEffect(() => {
+    setData(null);
+    setIsError(false);
     if (!url) return;
     const request = async () => {
-  
-      const response = await fetch(url);
-      const data = await response.json();
-      setData(data);
-    }
+      setIsLoading(true);
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setData(data);
+      } catch(e) {
+        setIsError(true);
+      }
+      setIsLoading(false);
+    };
     request();
   }, [url]);
-  return [setUrl, data]
+  return [setUrl, data, isLoading, isError];
 }
 
 function App() {
   const [name, setName] = useState("");
-  const [setUrl, data] = usePokeApi();
+  const [setUrl, data, isLoading, isError] = usePokeApi();
 
   return (
-    <form onSubmit={e => {
-      e.preventDefault();
-      setUrl(`${API}/pokemon/${name}`);
-    }}>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        setUrl(`${API}/pokemon/${name}`);
+      }}
+    >
       <input
         type="text"
         value={name}
@@ -36,7 +46,9 @@ function App() {
       />
       <input type="submit" value="search" />
       <div>We are searching for {name}</div>
-      <pre><code>{JSON.stringify(data, null, 2)}</code></pre>
+      {isError && <div>Something went wrong :(</div>}
+      {isLoading && <div>Loading...</div>}
+      {data && data.sprites && <img src={data.sprites["front_default"]} alt={data.name} />}
     </form>
   );
 }
