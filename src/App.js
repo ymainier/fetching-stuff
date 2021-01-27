6619b0e486665edc27429975f6f795d47090682c
Yann Mainier <yann.mainier@gmail.com>
2021-01-27 14:33:38 +0000

    first step

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+
+const API = "https://pokeapi.co/api/v2";
+
+function usePokeApi() {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [url, setUrl] = useState("");
+  useEffect(() => {
+    if (!url) return;
+    const request = async () => {
+  
+      const response = await fetch(url);
+      const data = await response.json();
+      setData(data);
+    }
+    request();
+  }, [url]);
+  return [setUrl, data]
+}
 
 function App() {
+  const [name, setName] = useState("");
+  const [setUrl, data] = usePokeApi();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={e => {
+      e.preventDefault();
+      setUrl(`${API}/pokemon/${name}`);
+    }}>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input type="submit" value="search" />
+      <div>We are searching for {name}</div>
+      <pre><code>{JSON.stringify(data, null, 2)}</code></pre>
+    </form>
   );
 }
 
